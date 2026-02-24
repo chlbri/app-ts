4e8473b966453ad6b647f934575bd7ff7753ff96
chlbri <51156116+chlbri@users.noreply.github.com>
2026-02-24 19:46:36 +0000

    ok before implements actors
    _NO_CI

--- src/actor.ts
+++ src/actor.ts
@@ -15,13 +15,15 @@ type EmitterConfig<Paths extends string = string> = CommonActor & {
 type PromiseeConfig<Paths extends string = string> = CommonActor & {
   // Max wait time to perform the promise
   readonly max?: string;
+  readonly id: string;
   readonly then: SingleOrArrayT<Paths>;
   readonly catch: SingleOrArrayT<Paths>;
   readonly finally?: FinallyConfig<Paths>;
 };
 
-type MachineConfig<Paths extends string = string> = CommonActor &
-  (
+type MachineConfig<Paths extends string = string> = CommonActor & {
+  readonly id: string;
+} & (
     | {
         readonly on: Record<string, SingleOrArrayT<Paths>>;
         readonly contexts?: Record<string, string>;
